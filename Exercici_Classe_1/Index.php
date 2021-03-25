eae2c0e5658393f653870e95e0e6bf03cbfa4686
lluisf6 <lluis.fernandez@mataro.epiaedu.cat>
2021-03-25 16:51:51 +0100

    all

---
+++ Exercici_Classe_1/Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+
+	<?php
+           echo "<h3 style=\"color: green;\"><i>Estic utilitzant el llenguatge de programació<b> PHP</b></i></h3>"
+        ?>
+</body>
+</html>
